5f63ca42193012c79768902b257d095e00e2fd88
DarkenWaves <108982982+DarkenWaves@users.noreply.github.com>
2022-09-03 00:51:00 +0200

    Add files via upload

--- Void.js
+++ Void.js
@@ -256,6 +256,7 @@ Here we are setting Levelling,With help of Module discord-Xp  https://www.npmjs.
 Works with MongoDb so no data loss.
 ╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺╺
 */
+    if (icmd) {
       const randomXp = 8;
       const hasLeveledUp = await Levels.appendXp(citel.sender, "RandomXP", randomXp);
       if (hasLeveledUp) {
